a3900cf978cb725389b1931d5fc21e5f9f0cc648
Android25 <smesser25@gmail.com>
2015-10-14 20:38:18 -0500

    Update to 1.7.10
    Client Proxy wasn't registering with mod due to mismatched types.

--- main/java/com/the_beast_unleashed/chunkpurge/proxy/ProxyClient.java
+++ main/java/com/the_beast_unleashed/chunkpurge/proxy/ProxyClient.java
@@ -1,31 +1,8 @@
 package com.the_beast_unleashed.chunkpurge.proxy;
 
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import com.the_beast_unleashed.chunkpurge.proxy.ProxyCommon;
 
-public class ProxyClient
+public class ProxyClient extends ProxyCommon
 {
-	
-	public void preInit(FMLPreInitializationEvent event)
-	{
-		
-	}
-	
-	public void load(FMLInitializationEvent event)
-	{
-
-	}
-	
-	public void postInit(FMLPostInitializationEvent event)
-	{
-
-	}
-	
-	public void serverLoad(FMLServerStartingEvent event)
-	{
-		
-	}
 	
 }
