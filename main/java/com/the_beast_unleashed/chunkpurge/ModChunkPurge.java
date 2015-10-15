9a7b174a8ec7b1bb00bc10e85b45d9ed462b13b6
Android25 <smesser25@gmail.com>
2015-10-14 20:41:27 -0500

    Update mod version to 2.0 for 1.7.10

--- main/java/com/the_beast_unleashed/chunkpurge/ModChunkPurge.java
+++ main/java/com/the_beast_unleashed/chunkpurge/ModChunkPurge.java
@@ -17,7 +17,7 @@ public class ModChunkPurge
 
 	public static final String MODID = "ChunkPurge";
 	public static final String NAME = "Chunk Purge";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "2.0";
 
 	@Instance(MODID)
 	public static ModChunkPurge instance;
